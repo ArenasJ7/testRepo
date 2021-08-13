e233393120e4ab5edfa244afa69d1c7fad586c31
Javier Arenas Sanchez <javier.arenas@cemex.com>
2021-08-12 19:53:37 -0500

    Added Difference Function

--- vanilla.py
+++ vanilla.py
@@ -10,11 +10,15 @@ def sum(x, y):
     print(x+y)
 
 
+def difference(x,y):
+    print(x-y)
+
 if __name__ == "__main__":
 
 	helloWorld()
     print("The sum is: ")
     sum(3,5)
+    difference(10,5)
 
 
 	
